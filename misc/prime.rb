ec97171b92a201b89d6de0bd6be6f27636b15cc0
samquigley <samq2011@live.com>
2018-01-30 22:14:41 +0000

    prime numbers script done with two variantions on algorithm#

--- misc/prime.rb
+++ misc/prime.rb
@@ -1,36 +1,24 @@
-#stack overflow example - iterate through values
-# def get_prime_no_upto(number)
-#     start = 2
-#     primes = (start..number).to_a
-#     (start..number).each do |no|
-#       (start..no).each do |num|
-#         if ( no % num  == 0) && num != no
-#           primes.delete(no)
-#           break
-#         end
-#       end
-#     end
-#     primes
-#   end
-# puts get_prime_no_upto(10000)
+require 'prime'
+require 'benchmark'
 
-# # modulo approach
-# def prime_modulo
-#   print ("Enter a number: ")
-#   num = gets()
-#   if(num % 2 == 0)
-#     print("#{num}is a prime number")
-#   else
-#     print("#{num}is NOT a prime number")
-#   end
-# end
-# print prime_modulo
+num=ARGV[0].to_i
 
 #square root aproach
-num=ARGV[0].to_i
 def prime_sqrt(num)
   return false if num <= 1
   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
   true
 end
-print prime_sqrt(num)
+
+#Ruby Prime
+def prime(num)
+  Prime.prime?(num)
+end
+
+
+n=2
+Benchmark.bm do |x|  
+  x.report(prime(num)) {prime(num)} 
+  x.report(prime_sqrt(num)) {prime_sqrt(num)}
+end
+
