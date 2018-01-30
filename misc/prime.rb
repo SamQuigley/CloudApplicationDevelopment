be49c842a52abd0b4ba9f018b2f97e7271698216
samquigley <samq2011@live.com>
2018-01-30 00:43:09 +0000

    house keeping

--- misc/prime.rb
+++ misc/prime.rb
@@ -1,30 +1,30 @@
 #stack overflow example - iterate through values
-def get_prime_no_upto(number)
-    start = 2
-    primes = (start..number).to_a
-    (start..number).each do |no|
-      (start..no).each do |num|
-        if ( no % num  == 0) && num != no
-          primes.delete(no)
-          break
-        end
-      end
-    end
-    primes
-  end
-puts get_prime_no_upto(10000)
+# def get_prime_no_upto(number)
+#     start = 2
+#     primes = (start..number).to_a
+#     (start..number).each do |no|
+#       (start..no).each do |num|
+#         if ( no % num  == 0) && num != no
+#           primes.delete(no)
+#           break
+#         end
+#       end
+#     end
+#     primes
+#   end
+# puts get_prime_no_upto(10000)
 
-# modulo approach
-def prime_modulo
-  print ("Enter a number: ")
-  num = gets()
-  if(num % 2 == 0)
-    print("#{num}is a prime number")
-  else
-    print("#{num}is NOT a prime number")
-  end
-end
-print prime_modulo
+# # modulo approach
+# def prime_modulo
+#   print ("Enter a number: ")
+#   num = gets()
+#   if(num % 2 == 0)
+#     print("#{num}is a prime number")
+#   else
+#     print("#{num}is NOT a prime number")
+#   end
+# end
+# print prime_modulo
 
 #square root aproach
 num=ARGV[0].to_i
